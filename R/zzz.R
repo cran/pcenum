e2a1ddac7bb35f025766ce78d03fd6dc39f4e470
Benjamin Auder <Benjamin.Auder@gmail.com>
2012-12-10 00:00:00 +0000

    version 1.0.1

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,9 @@
 #called when package is loaded :
-.First.lib <- function (lib, pkg)
-{
-  library.dynam("pcenum", pkg, lib)
-}
+#WARNING:: DEPRECATED
+#.First.lib <- function (lib, pkg)
+#{
+#  library.dynam("pcenum", pkg, lib)
+#}
 
 #called when package is unloaded ( detach("package:pkg_name") )
 .Last.lib <- function(path)
